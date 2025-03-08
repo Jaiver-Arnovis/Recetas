efb3e534fc10c58a8d02fd6fba16f3de7835ddd8
jaiver Arnovis <83389914+Jaiver-Arnovis@users.noreply.github.com>
2025-03-08 13:23:43 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    aplicarPreferenciaModoOscuro();
+    document.getElementById("toggleMode").addEventListener("click", toggleDarkMode);
+    cargarRecetas('postres.html'); // Cargar "Platos Fuertes" por defecto
+});
+
+function cargarRecetas(archivo) {
+    fetch(archivo)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('contenido').innerHTML = data;
+            aplicarPreferenciaModoOscuro();
+            configurarCambioDeRecetas();
+        })
+        .catch(error => console.error('Error al cargar las recetas:', error));
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+}
+
+function aplicarPreferenciaModoOscuro() {
+    if (localStorage.getItem("darkMode") === "true") {
+        document.body.classList.add("dark-mode");
+    }
+}
+
+function configurarCambioDeRecetas() {
+    let recetas = document.querySelectorAll(".receta");
+    let indiceReceta = 0;
+    
+    document.getElementById("siguienteReceta").addEventListener("click", () => {
+        recetas[indiceReceta].classList.remove("active");
+        indiceReceta = (indiceReceta + 1) % recetas.length;
+        recetas[indiceReceta].classList.add("active");
+    });
+}
